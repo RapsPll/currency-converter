3c23371941708590e4350cb54fcd3b19fd7b2f5e
Marcin Malinowski <mmalinowski265@wp.pl>
2020-09-10 12:30:42 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+{
+const formElement = document.querySelector(".js-form");
+const amountElement = document.querySelector(".js-amount");
+const currencyElement = document.querySelector(".js-currency");
+// const resultElement = document.querySelector(".");
+const button = document.querySelector(".js-button");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
+
+
+
+
+
+
+
+}
